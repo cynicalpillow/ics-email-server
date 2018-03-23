adb65591d7ea0f32fadf4d536febeb60e688ac44
Yuan Rui Li <cynicalrui@gmail.com>
2018-03-23 15:13:21 -0400

    Add files via upload

--- EmailServer.java
+++ EmailServer.java
@@ -1,11 +1,38 @@
 public class EmailServer{
+    public static void printMessage(Tree t){
+	if(t.getRoot() == null)return;
+	printMessage(new Tree(t.getRoot().getLeft()));
+	Message m = new Message();
+	int rN = t.getRoot().getRecordNumber();
+	m.readFromMessagesFile(rN);
+	System.out.println(m);
+	System.out.println("--------------");
+	printMessage(new Tree(t.getRoot().getRight()));
+    }
     public static void main(String args[]){
 	int error = Init.initializeSystem();
 	if(error == Globals.PROCESS_OK){
 	    Message message = new Message();
-	    int recordNumber = 0;
-	    String identification = Globals.STR_NULL;
+	    //Test 1
 	    Globals.senderIndex.printTree();
+	    int recordNumber = Globals.senderIndex.findNode("JB", 1).getRecordNumber();
+	    message.readFromMessagesFile(recordNumber);
+	    System.out.println(message);
+	    System.out.println("--------------");
+	    System.out.println("--------------");
+	    
+	    //Test 2
+	    printMessage(Globals.senderIndex);
+	    
+	    //Test 3
+	    TNode n = Globals.senderIndex.findNode("bJ", 1);
+	    if(n == null)System.out.println("Not found");
+	    else {
+		recordNumber = n.getRecordNumber();
+		message.readFromMessagesFile(recordNumber);
+		System.out.println(message);
+	    }
+	    
 	    FileIO.closeMessagesFile();
 	} else {
 	    Error.report(0);
